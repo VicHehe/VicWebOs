51b1cb01354234c8a3de978be2fdb8a8856d8983
Victor Tilleria <victortilleria116@gmail.com>
2026-09-18 12:49:22 -0300

    Update JsWidgets.js

--- js/JsWidgets.js
+++ js/JsWidgets.js
@@ -24,5 +24,38 @@ const WIDGETS_DISPONIBLES = [
         esBase: false,
         espacio: 4,
         monedas: 15
+    },
+    {
+        id: 'bubble-image',
+        nombre: 'Imagen Burbuja',
+        icono: 'circle',
+        ruta: 'Widgets/bubble-image/index.html',
+        descripcion: 'Muestra una imagen de tu galería en un círculo.',
+        categoria: 'Personalización',
+        esBase: false,
+        espacio: 4,
+        monedas: 0
+    },
+    {
+        id: 'ruleta',
+        nombre: 'Mini Ruleta',
+        icono: 'circle-dot-dashed',
+        ruta: 'Widgets/ruleta/index.html',
+        descripcion: 'Ruleta con hasta 5 opciones. Resultado al azar.',
+        categoria: 'Juegos',
+        esBase: false,
+        espacio: 4,
+        monedas: 25
+    },
+    {
+        id: 'acceso-directo',
+        nombre: 'Acceso Directo',
+        icono: 'link',
+        ruta: 'Widgets/acceso-directo/index.html',
+        descripcion: '4 atajos a páginas web con icono y nombre.',
+        categoria: 'Utilidades',
+        esBase: false,
+        espacio: 4,
+        monedas: 40
     }
 ];
