8ff4b6d3a1a080fcf2ae6605958bfecc69f07f19
Victor Tilleria <victortilleria116@gmail.com>
2026-09-19 06:01:22 -0300

    Update JsRutas.js

--- js/JsRutas.js
+++ js/JsRutas.js
@@ -115,6 +115,17 @@ const RUTAS_HERRAMIENTAS = [
     monedas: 0
 },
     {
+        id: 'ajedrez',
+        nombre: 'Ajedrez',
+        icono: 'crown',
+        ruta: 'herramientas/ajedrez/index.html',
+        descripcion: 'El juego de reyes. IA en dos niveles, recompensas por capturas y victoria.',
+        categoria: 'Juegos',
+        esBase: false,
+        espacio: 8,
+        monedas: 50
+    },
+    {
         id: 'evmail',
         nombre: 'EVmail',
         icono: 'mail',
