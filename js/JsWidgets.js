987dd8d711db267118871cec7c695dab1c3f984b
Victor Tilleria <victortilleria116@gmail.com>
2026-09-20 21:03:04 -0300

    Add account-info widget to JsWidgets.js
    Added a new widget for account information with details.

--- js/JsWidgets.js
+++ js/JsWidgets.js
@@ -104,6 +104,17 @@ const WIDGETS_DISPONIBLES = [
         espacio: 4,
         monedas: 60
     },
+        {
+        id: 'account-info',
+        nombre: 'Cuenta Info',
+        icono: 'id-card',
+        ruta: 'Widgets/account-info/index.html',
+        descripcion: 'Resumen de tu cuenta: perfil, dinero ganado/gastado y qué tenés instalado.',
+        categoria: 'Utilidades',
+        esBase: false,
+        espacio: 2,
+        monedas: 0
+    },
     {
         id: 'acceso-directo',
         nombre: 'Acceso Directo',
